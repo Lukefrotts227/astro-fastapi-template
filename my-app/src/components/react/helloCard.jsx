f91876e1ef6910c1262c86673f7c991788da8088
Lukas Frotton <132273525+Lukefrotts227@users.noreply.github.com>
2023-12-11 04:24:30 +0000

    further commit

--- my-app/src/components/react/helloCard.jsx
+++ my-app/src/components/react/helloCard.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'; 
 
 export default function HelloCard(){
-
+    const [change, setChange] = useState('begin'); 
+    
     return(
         <div>
-            
+            <h1>Welcome To The App</h1>
+            <button>Click here</button>
         </div>
     )
 }
