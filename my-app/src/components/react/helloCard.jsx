3f28cd752fa983d869a0737ba6f626e11ec12969
Lukas Frotton <132273525+Lukefrotts227@users.noreply.github.com>
2023-12-11 16:13:18 +0000

    starting the form

--- my-app/src/components/react/helloCard.jsx
+++ my-app/src/components/react/helloCard.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react'; 
 
 export default function HelloCard(){
-    const [change, setChange] = useState('begin'); 
+    const [change, setChange] = useState(false); 
+
+    const doChange = () =>{
+        if(change === false){
+            setChange(true); 
+        }else{
+            setChange(false);; 
+        }
+    }
+
     
     return(
-        <div>
-            <h1>Welcome To The App</h1>
-            <button>Click here</button>
+        <div class="flex flex-col items-center justify-center text-center bg-gradient-to-tr from-slate-500 to-slate-700 p-4 shadow-2xl rounded-md">
+            <h1 class="text-xl">Welcome To The App</h1>
+            <button onClick={doChange} class="bg-blue-200 p-2 m-3 rounded-full hover:bg-blue-400">Click here</button>
+            {change && <h1 class="text-md font-bold text-black">This is a react component</h1>}
         </div>
     )
 }
---
+++ my-app/src/components/react/reactForm.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'; 
+import axios from 'axios'; 
+
+export default function Form(){
+    
+    const handleSubmit = async (e) =>{
+
+    }
+
+    return(
+        <form onSubmit={handleSubmit} class="w-full max-w-lg bg-white rounded-lg shadow-md p-6">
+            
+        </form>
+
+    );
+}
